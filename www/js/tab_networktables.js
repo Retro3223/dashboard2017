430bba15441748866faa86eeab5143a586ae3dff
Ellery Newcomer <ellery-newcomer@utulsa.edu>
2017-02-08 08:00:02 -0800

    hook up network tables, cameras

---
+++ www/js/tab_networktables.js
@@ -0,0 +1,74 @@
+(function() {
+    function onValueChanged(key, value, isNew) {
+        if (isNew) {
+            //console.info('onValueChanged', key, value, isNew);
+            var table = '#nt > tbody:last';
+
+            var joystickKeys = ["right_", "left_"];
+
+            var sensorKeys = [ "seesHighGoal", "xOffsetHighGoal", "zOffsetHighGoal", "thetaHighGoal", ];
+
+            var recordKeys = ["recordMode", "recordName", "recordStatus", "recording"];
+
+            var parameterKeys = [];
+
+            for (var i = 0; i < joystickKeys.length; i++) {
+                for (var j = 0; j <= 12; j++) {
+                    if (key === "/SmartDashboard/" + joystickKeys[i] + j) {
+                        table = '#joystick_vars > tbody:last';
+                    }
+                }
+            }
+
+            for (var i = 0; i < sensorKeys.length; i++) {
+                if (key === "/SmartDashboard/" + sensorKeys[i]) {
+                    table = '#sensor_vars > tbody:last';
+                }
+            }
+
+            for (var i = 0; i < recordKeys.length; i++) {
+                if (key === "/SmartDashboard/" + recordKeys[i]) {
+                    table = '#record_vars > tbody:last';
+                }
+            }
+
+            for (var i = 0; i < parameterKeys.length; i++) {
+                if (key === "/SmartDashboard/" + parameterKeys[i]) {
+                    table = '#parameter_vars > tbody:last';
+                }
+            }
+
+            var tr = $('<tr></tr>').appendTo($(table));
+            $('<td></td>').text(key).appendTo(tr);
+            $('<td></td>').attr('id', NetworkTables.keyToId(key))
+                .text(value)
+                .appendTo(tr);
+
+        } else {
+            $('#' + NetworkTables.keySelector(key)).text(value);
+
+            if (key=== "/SmartDashboard/angle"){
+                $("#Compass .arrow").css("transform", "rotate(" + value + "deg)");
+                $("#CompassDisplay").text(numeral(value).format("0.00") + " degrees");
+            }
+            if (key === "/SmartDashboard/shooter_pitch") {
+                $("#daShooterArm").css("transform", "rotate(" + (-value) + "deg)");
+                $("#daShooterArmDisplay").text(numeral(value).format("0.00") + " degrees");
+            }
+
+        }
+
+        if (key === "/SmartDashboard/autonomousOver") {
+            autonomousOver();	
+        }
+
+        if (key === "/SmartDashboard/driveMode") {
+            $("#driveMode").text(value);
+
+        }
+    }
+
+    $(function() {
+        NetworkTables.addGlobalListener(onValueChanged, true);
+    });
+})();
